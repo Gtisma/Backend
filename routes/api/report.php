7b14b8b5156dd5b80f54eb690c953986b733dbab
Ezeibe Sandra Chioma <chioma@tm30.net>
2021-01-20 21:50:51 +0100

    Added new url

--- routes/api/report.php
+++ routes/api/report.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 Route::group( [ 'middleware' => [ 'auth:api'] ], function () {
-    Route::post( '/', 'Api\V1\ReportController@sendReport' )->name('store.reports');
+    Route::post( '/', 'Api\V1\ReportController@sendReport2' )->name('store.reports');
     Route::get( '/', 'Api\V1\ReportController@viewReport' )->name('get.reports');
     Route::post( '/approval', 'Api\V1\ReportController@approveReport' )->name('a[[rove.reports');
 });
