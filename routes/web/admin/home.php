dcdf8e01f0b68402302bfca168a029939d286491
Ezeibe Sandra Chioma <chioma@tm30.net>
2021-01-22 23:57:06 +0100

    Updated send Report option

--- routes/web/admin/home.php
+++ routes/web/admin/home.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::group( [ 'middleware' => [ 'auth','role:administrator|superadmin' ] ], function () {
 
     Route::get('/', 'Admin\HomeController@index')->name('admin.home');
+    Route::get('/version', function () {
+        dd(phpinfo());
+    } )->name( 'version' );
 });
+
