785aed217447745a98a8ac8ef698bb7fe87aad71
Ezeibe Sandra Chioma <chioma@tm30.net>
2021-01-24 16:13:55 +0100

    Added View List for component

--- routes/web/admin/rank.php
+++ routes/web/admin/rank.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group( [ 'middleware' => [ 'auth','role:administrator|superadmin' ] ], function () {
 
-    Route::get('/{id}', 'Admin\rankController@viewRanks')->name('admin.view.ranks');
+    Route::get('/{id}', 'Admin\RankController@viewRanks')->name('admin.view.ranks');
     Route::post('/add', 'Admin\UserController@viewUsers')->name('admin.view.addrakn');
 
 });
